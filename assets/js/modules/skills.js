5169ca7058b095ada4615d633eef25e355a05281
Ayrton Gallo <ayrton_gallo@outlook.com>
2026-07-14 01:54:51 -0300

    Polished the HTML monolith hehe

--- assets/js/modules/skills.js
+++
@@ -1,8 +0,0 @@
-export const skills = {
-    frontend: ["HTML","CSS", "Javascript"],
-    backend: ["Python", ".NET", "Java", "NodeJS"],
-    data: ["NumPy", "Pandas", "SQL", "Scikit-learn"],
-    math: ["Linear Algebra", "Econometrics", "Statistics", "Probability"],
-    cloud: ["Docker", "Kubernetes", "AWS", "Azure", "GCP"],
-    engineering: ["Agile Methodologies", "Team Work"]
-};
